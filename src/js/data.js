5b2c4a6649889ec573810982e2294d376dfcd179
Andrii Chyryk <achyryk86@gmail.com>
2019-12-08 23:36:14 +0200

    Upgrade serialize-javascript to version 2.1.1 - security alert

---
+++ src/js/data.js
@@ -0,0 +1,135 @@
+const data = [
+    {
+        "Airline": "Ukraine International",
+        "Aircraft": "KSEA",
+        "AirlineLogo": "https://www.skyscanner.net/images/airlines/small/LO.png",
+        "Location": "Kyiv",
+        "Destination": "Lviv"
+    },
+    {
+        "Airline": "Ukraine International",
+        "Aircraft": "KSEA",
+        "AirlineLogo": "https://www.skyscanner.net/images/airlines/small/LO.png",
+        "Location": "Kyiv",
+        "Destination": "Lviv"
+    },
+    {
+        "Airline": "Lufthansa",
+        "Aircraft": "LOA",
+        "AirlineLogo": "https://www.skyscanner.net/images/airlines/small/LH.png",
+        "Location": "Kyiv",
+        "Destination": "Lviv"
+    },
+    {
+        "Airline": "Ukraine International",
+        "Aircraft": "KSEA",
+        "AirlineLogo": "https://www.skyscanner.net/images/airlines/small/LO.png",
+        "Location": "Lviv",
+        "Destination": "Odessa"
+    },
+    {
+        "Airline": "Wizz Air",
+        "Aircraft": "KSEA",
+        "AirlineLogo": "https://www.skyscanner.net/images/airlines/small/WZ.png",
+        "Location": "Lviv",
+        "Destination": "Odessa"
+    },
+    {
+        "Airline": "Alaska Airlines",
+        "Aircraft": "FKED",
+        "Location": "Lviv",
+        "Destination": "Odessa"
+    },
+    {
+        "Airline": "KLM",
+        "Aircraft": "SDE",
+        "AirlineLogo": "https://www.skyscanner.net/images/airlines/small/KL.png",
+        "Location": "Lviv",
+        "Destination": "Kyiv"
+    },
+    {
+        "Airline": "Ukraine International",
+        "Aircraft": "KSEA",
+        "AirlineLogo": "https://www.skyscanner.net/images/airlines/small/LO.png",
+        "Location": "Lviv",
+        "Destination": "Kyiv"
+    },
+    {
+        "Airline": "Lufthansa",
+        "Aircraft": "LOA",
+        "AirlineLogo": "https://www.skyscanner.net/images/airlines/small/LH.png",
+        "Location": "Lviv",
+        "Destination": "Kyiv"
+    },
+    {
+        "Airline": "Ukraine International",
+        "Aircraft": "KSEA",
+        "AirlineLogo": "https://www.skyscanner.net/images/airlines/small/PS.png",
+        "Location": "Lviv",
+        "Destination": "Kharkiv"
+    },
+    {
+        "Airline": "Belavia",
+        "Aircraft": "LO",
+        "Location": "Lviv",
+        "Destination": "Kharkiv"
+    },
+    {
+        "Airline": "LOT",
+        "Aircraft": "DOCTA",
+        "AirlineLogo": "https://www.skyscanner.net/images/airlines/small/LO.png",
+        "Location": "Lviv",
+        "Destination": "Kharkiv"
+    },
+    {
+        "Airline": "AirBaltic",
+        "Aircraft": "FKED",
+        "AirlineLogo": "https://www.skyscanner.net/images/airlines/small/BT.png",
+        "Location": "Lviv",
+        "Destination": "Kharkiv"
+    },
+    {
+        "Airline": "LOT",
+        "Aircraft": "EIV",
+        "AirlineLogo": "https://www.skyscanner.net/images/airlines/small/LO.png",
+        "Location": "Lviv",
+        "Destination": "Kharkiv"
+    },
+    {
+        "Airline": "Ukraine International",
+        "Aircraft": "DAO",
+        "AirlineLogo": "https://www.skyscanner.net/images/airlines/small/PS.png",
+        "Location": "Lviv",
+        "Destination": "Kharkiv"
+    },
+    {
+        "Airline": "Turkish Airlines",
+        "Aircraft": "KSEA",
+        "AirlineLogo": "https://www.skyscanner.net/images/airlines/small/TK.png",
+        "Location": "Lviv",
+        "Destination": "Kyiv"
+    },
+    {
+        "Airline": "Turkish Airlines",
+        "Aircraft": "KSEA",
+        "AirlineLogo": "https://www.skyscanner.net/images/airlines/small/TK.png",
+        "Location": "Lviv",
+        "Destination": "Kyiv"
+    },
+    {
+        "Airline": "Turkish Airlines",
+        "Aircraft": "FKED",
+        "AirlineLogo": "https://www.skyscanner.net/images/airlines/small/TK.png",
+        "Location": "Lviv",
+        "Destination": "Odessa"
+    },
+    {
+        "Airline": "Ukraine International",
+        "Aircraft": "DAO",
+        "AirlineLogo": "https://www.skyscanner.net/images/airlines/small/PS.png",
+        "Location": "Lviv",
+        "Destination": "Kyiv"
+    }
+];
+
+export { data } // Export data to index.js
